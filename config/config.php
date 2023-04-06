bf7614ec289c6d01509d749dffc845a2fd349136
H0ll0w0 <miguelguysantos@gmail.com>
2023-04-06 11:37:03 +0200

    hello

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('TEMPLATE', './template');
+define('TEMPLATE_PARTS', './template/template_parts');
+define('PAGES', './template/pages/');
+define('IMAGES', './assets/img');
+define('CSS', './assets/css');
+define('JS', './assets/js');
+define('TITLE', 'Pierre, Paul, Jacques');
+
+define('FONCTIONS', './fonctions');
+define('CLASSES', './classes');
